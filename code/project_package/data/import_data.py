104a8c1068877c0f95506e476b65cacba7b6ea35
tgoelles <thomas.goelles@gmail.com>
2024-05-16 07:31:56 +0000

    chore: Process and save latest Graz dataset as netCDF

--- code/project_package/data/import_data.py
+++ code/project_package/data/import_data.py
@@ -2,16 +2,20 @@ from project_package.data import config
 from pathlib import Path
 
 import pandas as pd
+import xarray as xr
 
 
 # Enter code here for the final generation of processed data
 def process_rawdata() -> None:
     """Convert all datasets and saves them in the intermediate folder"""
     # code comes here
-    demo_file = config.RAW_DATA_FOLDER / "demo.csv"
-    demo_df = pd.read_csv(demo_file)
-    # delite ID column
-    demo_df.drop("ID", axis=1, inplace=True)
-    demo_df.to_csv(config.INTERMEDIATE_DATA_FOLDER / "demo_clean.csv", index=False)
+    process_graz()
     print("Processed data saved to intermediate folder")
     return None
+
+
+def process_graz():
+    graz_file = config.RAW_DATA_FOLDER / "graz_may_2024_inca_20240501T0000_20240515T2300.nc"
+    graz = xr.open_dataset(graz_file)
+    graz_latest = graz.isel(time=-1)
+    graz_latest.to_netcdf(config.INTERMEDIATE_DATA_FOLDER / "graz_latest.nc")
